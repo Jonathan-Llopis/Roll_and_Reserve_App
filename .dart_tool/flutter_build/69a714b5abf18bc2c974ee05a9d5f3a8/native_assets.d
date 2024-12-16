caf2ddb9e1a077926c1d80b80646177f2c465e5b
Jonathan Llopis <jonathan.llopis.linares@gmail.com>
2024-12-16 13:05:08 +0100

    Flutter CLean

--- .dart_tool/flutter_build/69a714b5abf18bc2c974ee05a9d5f3a8/native_assets.d
+++
@@ -1,1 +0,0 @@
- /home/caveman96/DAM/projecteFinal/roll_and_reserve_app/.dart_tool/flutter_build/69a714b5abf18bc2c974ee05a9d5f3a8/native_assets.yaml: 
--- .dart_tool/flutter_build/6f29037a58e4b24f40c973208d57dfbe/native_assets.d
+++
@@ -1,1 +0,0 @@
- /home/caveman96/DAM/projecteFinal/roll_and_reserve_app/.dart_tool/flutter_build/6f29037a58e4b24f40c973208d57dfbe/native_assets.yaml: 
